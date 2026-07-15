fe2ddb64ae33fc6c5cacfc50b7a6efb45dc39ef4
attahDev <attahumana007@gmail.com>
2026-07-15 19:19:37 +0100

    Update CORS origin for production environment

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://gmbt-fe.vercel.app'],
+    origin: [
+      'http://localhost:5173',
+      'https://gmbtefro-pfst.vercel.app',
+    ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
